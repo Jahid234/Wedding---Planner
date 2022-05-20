9af0a0b0736072f5c86bd9116f5412e1e02e0f8d
Md. Jahidul Islam <97834703+Jahid234@users.noreply.github.com>
2022-05-20 23:20:54 +0600

    Add files via upload

---
+++ Wed.Plan/footer.php
@@ -0,0 +1,57 @@
+<?php @include 'home.php'; ?>
+
+
+<section class="footer">
+
+<div class="box-container">
+
+    <div class="box">
+        <h3>quick links</h3>
+        <a href="index.php"> <i class="fas fa-angle-right"></i> home</a>
+        <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+        <a href="clients.php"> <i class="fas fa-angle-right"></i> Clients</a>
+        <a href="team.php"> <i class="fas fa-angle-right"></i> team</a>
+        <a href="payment.php"> <i class="fas fa-angle-right"></i> Payment</a>
+        <a href="contact.php"> <i class="fas fa-angle-right"></i> contact</a>
+        <a href="blog.php"> <i class="fas fa-angle-right"></i> blog</a>
+        <a href="sign in.php"> <i class="fas fa-angle-right"></i> sign in</a>
+    </div>
+
+    <div class="box">
+        <h3>extra links</h3>
+        <a href="#"> <i class="fas fa-angle-right"></i> plan wedding</a>
+        <a href="#"> <i class="fas fa-angle-right"></i> our services</a>
+        <a href="#"> <i class="fas fa-angle-right"></i> our team</a>
+        <a href="#"> <i class="fas fa-angle-right"></i> our clients</a>
+        <a href="#"> <i class="fas fa-angle-right"></i> our payment</a>
+        <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+        <a href="#"> <i class="fas fa-angle-right"></i> terms of use</a>
+        <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+    </div>
+
+    <div class="box">
+        <h3>contact info</h3>
+        <a href="#"> <i class="fas fa-phone"></i> 01850505548 </a>
+        <a href="#"> <i class="fas fa-phone"></i> 01741636255 </a>
+        <a href="#"> <i class="fas fa-phone"></i> 06298 </a>
+        <a href="#"> <i class="fas fa-envelope"></i> jahid1213cvgc@gmail.com </a>
+        <a href="#"> <i class="fas fa-envelope"></i> jahidneub23@gmail.com </a>
+        <a href="#"> <i class="fas fa-envelope"></i> jahidneub_2020s@yahoo.com </a>
+        <a href="#"> <i class="fas fa-map"></i> North East University Bangladesh </a>
+        <a href="#"> <i class="fas fa-map"></i> Sylhet Sadar, Sylhet, Bangladesh </a>
+    </div>
+
+    <div class="box">
+        <h3>follow us</h3>
+        <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+        <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+        <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+        <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+        <a href="#"> <i class="fab fa-github"></i> github </a>
+    </div>
+
+</div>
+
+<p class="credit"> created by | <span>Md. Jahidul Islam</span> | all rights reserved! </p>
+
+</section>
