f95e8f8747eb1b8d125cb3db45e47112aa5e7e0e
Md. Jahidul Islam <97834703+Jahid234@users.noreply.github.com>
2022-05-25 22:26:36 +0600

    Update clients.php

--- clients.php
+++ clients.php
@@ -4,7 +4,7 @@
 <head>    
     <title> Clients </title>
 
-    <!-- font awesome cdn link  -->
+    <!-- font cdn link  -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
 
     <!-- css file link -->
@@ -16,60 +16,59 @@
 
     <?php @include 'header.php'; ?>
 
-    <?php @include 'home.php'; ?>
+    <!-- client section starts  -->
 
+    <section class="clients" id="clients">
 
-    
-<!-- client section starts  -->
-
-<section class="clients" id="clients">
-
-<div class="heading">
-    <h1>our previous clients</h1>
-    <img src="header-img.png" alt="">
-</div>
-
-<div class="row">
-
-    <div class="box-container">
-        <div class="box">
-            <img src="client-1.jpg" alt="">
-            <div class="info">
-                <h3>01. mark & alice</h3>
-                <p> <i class="fas fa-quote-left"></i> Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet illo aut repudiandae nihil quidem perspiciatis natus placeat, inventore, vitae, quia totam ad dolores velit. Cumque, alias ullam. Nihil, aperiam hic! <i class="fas fa-quote-right"></i> </p>
-            </div>
+        <div class="heading">
+            <h1>our previous clients</h1>
+            <img src="img/header-img.png" alt="">
         </div>
-        <div class="box">
-            <img src="client-2.jpg" alt="">
-            <div class="info">
-                <h3>02. Joe & alica</h3>
-                <p> <i class="fas fa-quote-left"></i> Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet illo aut repudiandae nihil quidem perspiciatis natus placeat, inventore, vitae, quia totam ad dolores velit. Cumque, alias ullam. Nihil, aperiam hic! <i class="fas fa-quote-right"></i> </p>
+
+        <div class="row">
+            <div class="box-container">
+
+                <div class="box">
+                    <img src="img/client-1.jpg" alt="">
+                    <div class="info">
+                        <h3>01. mark & alice</h3>
+                        <p> <i class="fas fa-quote-left"></i> Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet illo aut repudiandae nihil quidem perspiciatis natus placeat, inventore, vitae, quia totam ad dolores velit. Cumque, alias ullam. Nihil, aperiam hic! <i class="fas fa-quote-right"></i> </p>
+                    </div>
+                </div>
+                
+                <div class="box">
+                    <img src="img/clients-2.jpg" alt="">
+                    <div class="info">
+                        <h3>02. Joe & alica</h3>
+                        <p> <i class="fas fa-quote-left"></i> Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet illo aut repudiandae nihil quidem perspiciatis natus placeat, inventore, vitae, quia totam ad dolores velit. Cumque, alias ullam. Nihil, aperiam hic! <i class="fas fa-quote-right"></i> </p>
+                    </div>
+                </div>
+
+            </div>        
+
+            <div class="box-container">
+                <div class="box">
+                    <div class="info">
+                        <h3>03. mark & Henry</h3>
+                        <p> <i class="fas fa-quote-left"></i> Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet illo aut repudiandae nihil quidem perspiciatis natus placeat, inventore, vitae, quia totam ad dolores velit. Cumque, alias ullam. Nihil, aperiam hic! <i class="fas fa-quote-right"></i> </p>
+                    </div>
+                    <img src="img/clients-3.jpg" alt="">
+                </div>
+        
+                <div class="box">
+                    <div class="info">
+                        <h3>04. John & kits</h3>
+                        <p> <i class="fas fa-quote-left"></i> Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet illo aut repudiandae nihil quidem perspiciatis natus placeat, inventore, vitae, quia totam ad dolores velit. Cumque, alias ullam. Nihil, aperiam hic! <i class="fas fa-quote-right"></i> </p>
+                    </div>
+                    <img src="img/clients-4.jpg" alt="">
+                </div>
             </div>
+
         </div>
-    </div>        
 
-    <div class="box-container">
-        <div class="box">
-            <div class="info">
-                <h3>03. mark & Henry</h3>
-                <p> <i class="fas fa-quote-left"></i> Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet illo aut repudiandae nihil quidem perspiciatis natus placeat, inventore, vitae, quia totam ad dolores velit. Cumque, alias ullam. Nihil, aperiam hic! <i class="fas fa-quote-right"></i> </p>
-            </div>
-            <img src="client-3.jpg" alt="">
-        </div>
-        <div class="box">
-            <div class="info">
-                <h3>04. John & kits</h3>
-                <p> <i class="fas fa-quote-left"></i> Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet illo aut repudiandae nihil quidem perspiciatis natus placeat, inventore, vitae, quia totam ad dolores velit. Cumque, alias ullam. Nihil, aperiam hic! <i class="fas fa-quote-right"></i> </p>
-            </div>
-            <img src="client-4.jpg" alt="">
-        </div>
-    </div>
+    </section>
 
-</div>
-
-</section>
-
-<!-- client section ends -->
+    <!-- client section ends -->
 
 
     <!-- Scripts -->
