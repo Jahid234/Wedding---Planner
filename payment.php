c187d7706b0d7a86387e02850ced2227de675943
Md. Jahidul Islam <97834703+Jahid234@users.noreply.github.com>
2022-05-25 22:40:58 +0600

    Update payment.php

--- payment.php
+++ payment.php
@@ -16,64 +16,60 @@
 
     <?php @include 'header.php'; ?>
 
-    <?php @include 'home.php'; ?>
-
-
-
     <!-- Payment section starts -->
 
     <section class="Payment" id="payment">
-        <h1 class>our pricing</h1>
+        <h1 class>our pricing Criteria</h1>
 
         <div class="box-container">
 
             <div class="box">
-                <h3>basic plan</h3>
+                <h3> <b> basic plan </b> </h3>
                 <div class="price">$450/-</div>
                 <div class="list">
-                    <p> <i class="fas fa-check"></i> photography</p>
-                    <p> <i class="fas fa-check"></i> bribe makeup</p>
-                    <p> <i class="fas fa-check"></i> wedding ceromony</p>
-                    <p> <i class="fas fa-check"></i> wedding cake</p>
-                    <p> <i class="fas fa-check"></i> wedding registory</p>
-                    <p> <i class="fas fa-check"></i> meals & drinks</p>
-                    <p> <i class="fas fa-check"></i> guest invitations</p>
+                    <p> <i class="fas fa-check"></i> <b> photography </b> </p>
+                    <p> <i class="fas fa-check"></i> <b> bribe makeup </b> </p>
+                    <p> <i class="fas fa-check"></i> <b> wedding ceromony </b> </p>
+                    <p> <i class="fas fa-check"></i> <b> wedding cake </b> </p>
+                    <p> <i class="fas fa-check"></i> <b> wedding registory </b> </p>
+                    <p> <i class="fas fa-check"></i> <b> meals & drinks </b> </p>
+                    <p> <i class="fas fa-check"></i> <b> guest invitations </b> </p>
                 </div>
                 <a href="contact.php" class="btn">choose plan</a>
             </div>
 
             <div class="box">
-                <h3>premium plan</h3>
+                <h3><b> premium plan </b> </h3>
                 <div class="price">$850/-</div>
                 <div class="list">
-                <p> <i class="fas fa-check"></i> photography</p>
-                    <p> <i class="fas fa-check"></i> bribe makeup</p>
-                    <p> <i class="fas fa-check"></i> wedding ceromony</p>
-                    <p> <i class="fas fa-check"></i> wedding cake</p>
-                    <p> <i class="fas fa-check"></i> wedding registory</p>
-                    <p> <i class="fas fa-check"></i> meals & drinks</p>
-                    <p> <i class="fas fa-check"></i> guest invitations</p>
+                    <p> <i class="fas fa-check"></i> <b> photography </b> </p>
+                    <p> <i class="fas fa-check"></i> <b> bribe makeup </b> </p>
+                    <p> <i class="fas fa-check"></i> <b> wedding ceromony </b> </p>
+                    <p> <i class="fas fa-check"></i> <b> wedding cake </b> </p>
+                    <p> <i class="fas fa-check"></i> <b> wedding registory </b> </p>
+                    <p> <i class="fas fa-check"></i> <b> meals & drinks </b> </p>
+                    <p> <i class="fas fa-check"></i> <b> guest invitations </b> </p>
                 </div>
                 <a href="contact.php" class="btn">choose plan</a>
             </div>
 
             <div class="box">
-                <h3>ultimate plan</h3>
+                <h3><b> ultimate plan </b> </h3>
                 <div class="price">$1250/-</div>
                 <div class="list">
-                <p> <i class="fas fa-check"></i> photography</p>
-                    <p> <i class="fas fa-check"></i> bribe makeup</p>
-                    <p> <i class="fas fa-check"></i> wedding ceromony</p>
-                    <p> <i class="fas fa-check"></i> wedding cake</p>
-                    <p> <i class="fas fa-check"></i> wedding registory</p>
-                    <p> <i class="fas fa-check"></i> meals & drinks</p>
-                    <p> <i class="fas fa-check"></i> guest invitations</p>
+                    <p> <i class="fas fa-check"></i> <b> photography </b> </p>
+                    <p> <i class="fas fa-check"></i> <b> bribe makeup </b> </p>
+                    <p> <i class="fas fa-check"></i> <b> wedding ceromony </b> </p>
+                    <p> <i class="fas fa-check"></i> <b> wedding cake </b> </p>
+                    <p> <i class="fas fa-check"></i> <b> wedding registory </b> </p>
+                    <p> <i class="fas fa-check"></i> <b> meals & drinks </b> </p>
+                    <p> <i class="fas fa-check"></i> <b> guest invitations </b> </p>
                 </div>
                 <a href="contact.php" class="btn">choose plan</a>
             </div>
 
         </div>
-    </section>
+    </section>   
 
 
     <!-- Scripts -->
