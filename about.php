273a87802775a578b20a7f32fbe7d128302b928b
Md. Jahidul Islam <97834703+Jahid234@users.noreply.github.com>
2022-05-25 22:23:52 +0600

    Update about.php

--- about.php
+++ about.php
@@ -14,10 +14,7 @@
 
 <body>
 
-    <?php @include 'header.php'; ?>
-
-    <?php @include 'home.php'; ?>
-
+    <?php @include 'header.php'; ?>   
 
     <!-- About section starts -->
 
@@ -25,22 +22,22 @@
 
         <div class="heading">
             <h1>wedding about</h1>
-            <img src="header-img.png" alt="">
+            <img src="img/header-img.png" alt="">
         </div>
 
         <div class="box-container">
 
             <div class="box">
-                <img src="img1.jpg" alt="">
+                <img src="img/img1.jpg" alt="">
                 <div class="info">
                     <h3>dressing</h3>
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam, dolores.</p>
-                    <a href="#" class="btn">Learn more</a>
+                    <a href="team.php" class="btn">Learn more</a>
                 </div>
             </div>
 
             <div class="box">
-                <img src="img2.jpg" alt="">
+                <img src="img/img2.jpg" alt="">
                 <div class="info">
                     <h3>Florist</h3>
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam, dolores.</p>
@@ -49,7 +46,7 @@
             </div>
 
             <div class="box">
-                <img src="img3.jpg" alt="">
+                <img src="img/img3.jpg" alt="">
                 <div class="info">
                     <h3>wedding cake</h3>
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam, dolores.</p>
@@ -58,7 +55,7 @@
             </div>
 
             <div class="box">
-                <img src="img4.jpg" alt="">
+                <img src="img/img4.jpg" alt="">
                 <div class="info">
                     <h3>Outground Venue</h3>
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam, dolores.</p>
@@ -67,7 +64,7 @@
             </div>
 
             <div class="box">
-                <img src="img7.jpg" alt="">
+                <img src="img/img7.jpg" alt="">
                 <div class="info">
                     <h3>Inground Venue</h3>
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam, dolores.</p>
@@ -77,7 +74,7 @@
 
 
             <div class="box">
-                <img src="img5.jpg" alt="">
+                <img src="img/img5.jpg" alt="">
                 <div class="info">
                     <h3>music and party</h3>
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam, dolores.</p>
@@ -86,7 +83,7 @@
             </div>
 
             <div class="box">
-                <img src="img6.jpg" alt="">
+                <img src="img/img6.jpg" alt="">
                 <div class="info">
                     <h3>photographer</h3>
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam, dolores.</p>
@@ -96,7 +93,7 @@
 
 
             <div class="box">
-                <img src="img8.jpg" alt="">
+                <img src="img/img8.jpg" alt="">
                 <div class="info">
                     <h3>bribe-makeup</h3>
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam, dolores.</p>
@@ -105,7 +102,7 @@
             </div>
 
             <div class="box">
-                <img src="img9.jpg" alt="">
+                <img src="img/img9.jpg" alt="">
                 <div class="info">
                     <h3>meal and drinks</h3>
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam, dolores.</p>
@@ -114,7 +111,7 @@
             </div>
 
             <div class="box">
-                <img src="img10.jpg" alt="">
+                <img src="img/img10.jpg" alt="">
                 <div class="info">
                     <h3>wedding registory</h3>
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam, dolores.</p>
@@ -123,7 +120,7 @@
             </div>
 
             <div class="box">
-                <img src="img11.jpg" alt="">
+                <img src="img/img11.jpg" alt="">
                 <div class="info">
                     <h3>guest list</h3>
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam, dolores.</p>
@@ -132,7 +129,7 @@
             </div>
 
             <div class="box">
-                <img src="img12.jpg" alt="">
+                <img src="img/img12.jpg" alt="">
                 <div class="info">
                     <h3>fine dining</h3>
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam, dolores.</p>
@@ -144,7 +141,7 @@
 
     </section>
 
-    <!-- About section ends -->
+    <!-- About section ends -->  
 
 
 
