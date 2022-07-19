60c1e0a540969720f5955ba48e663426aa2d4c37
Md. Jahidul Islam <97834703+Jahid234@users.noreply.github.com>
2022-07-19 22:40:55 +0600

    Add files via upload

---
+++ about.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>    
+    <title> ABOUT </title>
+
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+
+    <!-- swiper css link  -->
+   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+    
+    <!-- css file link -->
+    <link rel="stylesheet" href="style.css">    
+
+</head>
+
+<body>
+
+    <?php @include 'header.php'; ?>
+
+    <!-- About section starts -->
+
+    <section class="about" id="about">
+
+        <div class="heading">
+            <h1>wedding about</h1>
+            <img src="img/header-img.png" alt="">
+        </div>
+
+        <div class="box-container">
+
+            <div class="box">
+                <img src="img/img1.jpg" alt="">
+                <div class="info">
+                    <h3>dressing</h3>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam, dolores.</p>
+                    <a href="team.php" class="btn">Learn more</a>
+                </div>
+            </div>
+
+            <div class="box">
+                <img src="img/img2.jpg" alt="">
+                <div class="info">
+                    <h3>Florist</h3>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam, dolores.</p>
+                    <a href="#" class="btn">Learn more</a>
+                </div>
+            </div>
+
+            <div class="box">
+                <img src="img/img3.jpg" alt="">
+                <div class="info">
+                    <h3>wedding cake</h3>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam, dolores.</p>
+                    <a href="#" class="btn">Learn more</a>
+                </div>
+            </div>
+
+            <div class="box">
+                <img src="img/img4.jpg" alt="">
+                <div class="info">
+                    <h3>Outground Venue</h3>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam, dolores.</p>
+                    <a href="#" class="btn">Learn more</a>
+                </div>
+            </div>
+
+            <div class="box">
+                <img src="img/img7.jpg" alt="">
+                <div class="info">
+                    <h3>Inground Venue</h3>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam, dolores.</p>
+                    <a href="#" class="btn">Learn more</a>
+                </div>
+            </div>
+
+
+            <div class="box">
+                <img src="img/img5.jpg" alt="">
+                <div class="info">
+                    <h3>music and party</h3>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam, dolores.</p>
+                    <a href="#" class="btn">Learn more</a>
+                </div>
+            </div>
+
+            <div class="box">
+                <img src="img/img6.jpg" alt="">
+                <div class="info">
+                    <h3>photographer</h3>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam, dolores.</p>
+                    <a href="#" class="btn">Learn more</a>
+                </div>
+            </div>
+
+
+            <div class="box">
+                <img src="img/img8.jpg" alt="">
+                <div class="info">
+                    <h3>bribe-makeup</h3>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam, dolores.</p>
+                    <a href="#" class="btn">Learn more</a>
+                </div>
+            </div>
+
+            <div class="box">
+                <img src="img/img9.jpg" alt="">
+                <div class="info">
+                    <h3>meal and drinks</h3>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam, dolores.</p>
+                    <a href="#" class="btn">Learn more</a>
+                </div>
+            </div>
+
+            <div class="box">
+                <img src="img/img10.jpg" alt="">
+                <div class="info">
+                    <h3>wedding registory</h3>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam, dolores.</p>
+                    <a href="#" class="btn">Learn more</a>
+                </div>
+            </div>
+
+            <div class="box">
+                <img src="img/img11.jpg" alt="">
+                <div class="info">
+                    <h3>guest list</h3>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam, dolores.</p>
+                    <a href="#" class="btn">Learn more</a>
+                </div>
+            </div>
+
+            <div class="box">
+                <img src="img/img12.jpg" alt="">
+                <div class="info">
+                    <h3>fine dining</h3>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam, dolores.</p>
+                    <a href="#" class="btn">Learn more</a>
+                </div>
+            </div>
+
+        </div>
+
+    </section>
+
+    <!-- About section ends -->
+
+    <!-- Scripts -->
+
+    <script src="script.js"></script>
+    
+
+</body>
+
+</html>
